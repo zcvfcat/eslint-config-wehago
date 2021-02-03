156299379f09baa57d6751639353bb42557b564f
김대현 <zcvfcat@gmail.com>
2021-02-04 08:50:47 +0900

    지만씨 요청 no-explicit-any : error

--- config/@typescript-eslint.js
+++ config/@typescript-eslint.js
@@ -18,7 +18,7 @@ module.exports = {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-explicit-any': ['error', { ignoreRestArgs: true }],
     '@typescript-eslint/no-empty-function': 'warn',
     '@typescript-eslint/no-use-before-define': [
       'warn',
